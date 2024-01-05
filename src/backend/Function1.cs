f196fbddbf9cc6f2c1e7b96b65d65f2e84325b4d
SergeyBokhantsev <sergey.bokhantsev@invisiblecrm.com>
2024-01-05 17:59:05 +0200

    authorize test

--- src/backend/Function1.cs
+++ src/backend/Function1.cs
@@ -1,4 +1,7 @@
 using System.Net;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -14,17 +17,23 @@ namespace FunctionApp
             _logger = loggerFactory.CreateLogger<Function1>();
         }
 
+        [Authorize]
         [Function("Function1")]
-        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req, ClaimsPrincipal claimsPrincipal)
         {
+            if (claimsPrincipal?.Identity?.IsAuthenticated != true)
+            {
+                return new UnauthorizedResult();
+            }
+
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            //var response = req.CreateResponse(HttpStatusCode.OK);
+            //response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            //response.WriteString("Welcome to Azure Functions!");
 
-            return response;
+            return new OkResult();
         }
     }
 }
