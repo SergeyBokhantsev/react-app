18804e2ff36724d2de5b537ae155e5271f833a01
SergeyBokhantsev <sergey.bokhantsev@invisiblecrm.com>
2024-01-05 20:18:51 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {process.env.REACT_APP_Var1} <code>src/App.js</code> and save {process.env.REACT_APP_Var2} to reload.
+          {process.env.REACT_APP_VAR1} <code>src/App.js</code> and save {process.env.REACT_APP_VAR2} to reload.
         </p>
         <a
           className="App-link"
